91fdd03e4b00819ca6cb4de4857f59b6eba93bba
WilliamEdwardZhang <willedwazhang@gmail.com>
2016-07-31 15:26:42 +0800

    init again

--- JavaScript/time.js
+++ JavaScript/time.js
@@ -1,12 +1,7 @@
-function clock() {
+﻿function clock() {
 	var txt=document.getElementById("time");
 	var itime=new Date();
-<<<<<<< HEAD
-	txt.innerHTML="当前时间:"+itime.getFullYear()+"年"+itime.getMonth()+1+"月"+itime.getDate()+
-=======
-	txt.innerHTML="当前时间:"+itime.getFullYear()+"年"+itime.getMonth()+"月"+itime.getDate()+
->>>>>>> parent of 2e7f37c... using English and add IP information.
-		"日"+itime.getHours()+"时"+itime.getMinutes()+"分"+itime.getSeconds()+"秒";
+	txt.innerHTML="Year: "+itime.getFullYear()+" Month: "+itime.getMonth()+" Day: "+itime.getDate()+" Time: "+itime.getHours()+"."+itime.getMinutes()+"."+itime.getSeconds();
 	setTimeout(clock, 500);
 }
 clock();
