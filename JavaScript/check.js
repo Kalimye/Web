2e7f37cf1c47c6a8a3f9bb72afbbdcaf03f29012
WilliamEdwardZhang <willedwazhang@gmail.com>
2016-07-31 14:29:47 +0800

    using English and add IP information.

--- JavaScript/check.js
+++ JavaScript/check.js
@@ -1,27 +1,27 @@
 function screensize() {
 
 	var txt=document.getElementById("No.1");
-	txt.innerHTML="屏幕大小为:高:"+innerHeight+";宽:"+innerWidth;
+	txt.innerHTML="Your Screen:Height:"+innerHeight+";Width:"+innerWidth;
 
 	setTimeout(screensize,1000)
 }
 
-document.write("<p>浏览器：")
+document.write("<p>Browser：")
 document.write(navigator.appName + "</p>")
 
-document.write("<p>浏览器版本：")
+document.write("<p>Browser Versions：")
 document.write(navigator.appVersion + "</p>")
 
-document.write("<p>代码：")
+document.write("<p>Code：")
 document.write(navigator.appCodeName + "</p>")
 
-document.write("<p>平台：")
+document.write("<p>Platform：")
 document.write(navigator.platform + "</p>")
 
-document.write("<p>Cookies 启用：")
+document.write("<p>Cookies isEnabled：")
 document.write(navigator.cookieEnabled + "</p>")
 
-document.write("<p>浏览器的用户代理报头：")
+document.write("<p>Your Agent：")
 document.write(navigator.userAgent + "</p>")
 
 screensize();
