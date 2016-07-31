fd84e69146b2c2f55ea637270ba5866b8dd21469
WilliamEdwardZhang <willedwazhang@gmail.com>
2016-07-31 15:21:32 +0800

    Update

--- JavaScript/time.js
+++ JavaScript/time.js
@@ -1,7 +1,7 @@
 function clock() {
 	var txt=document.getElementById("time");
 	var itime=new Date();
-	txt.innerHTML="当前时间:"+itime.getFullYear()+"年"+itime.getMonth()+"月"+itime.getDate()+
+	txt.innerHTML="当前时间:"+itime.getFullYear()+"年"+itime.getMonth()+1+"月"+itime.getDate()+
 		"日"+itime.getHours()+"时"+itime.getMinutes()+"分"+itime.getSeconds()+"秒";
 	setTimeout(clock, 500);
 }
